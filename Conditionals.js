3a3633457ddae4ea9ee6d8e3142c08fb1d67a6a5
FransiskusAgapa <137876704+FransiskusAgapa@users.noreply.github.com>
2023-06-27 17:51:13 -0700

    Update Conditionals.js

--- Conditionals.js
+++ Conditionals.js
@@ -2,6 +2,24 @@
 // 06/27/23
 // Student's assignment rewarding conditional 
 
+/*
+MVP
+As a diligent student, I want to reward myself if I finish my homework, and based on the time of day:
+
+I want a latte if it's before 10 am
+I want a hot chocolate it's between 10 am and 4 pm
+I want ice cream between 4 pm - 10 pm.
+If it's after 10 pm, I don't want anything other than sleep!
+Feature 1
+Building off the MVP, if I'm up for ice cream, I want strawberry - but only if it's Wednesday. Otherwise, I want vanilla.
+
+Feature 2 (hard) - Optional
+Building off Feature 1, I want to adjust the current conditions and add a new option so that if the time is between 3 pm - 6 pm, I want to have it pick either ice cream or hot chocolate depending on if the time is even or odd.
+ 
+Feature 3 (super hard) - Optional
+Building off Feature 2, I want my options for the 3 pm - 6 pm slot to be a random selection: if the time is even, I want my selections to be ice cream, cookies, or candy. If the time is odd, I want my selections to be hot chocolate, tea, or cake.
+*/
+
 let finishTime = 16; // student's input time
 let today = "Wednesday";
 
